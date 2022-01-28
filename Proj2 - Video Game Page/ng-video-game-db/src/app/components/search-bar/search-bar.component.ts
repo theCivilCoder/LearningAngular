13aeb79fc26912c19fe46f3585f7e9c8c55508eb
datascijim <yongjunjz@hotmail.com>
2022-01-27 21:43:38 -0700

    Search Bar Component - cleaned up code. search bar is visible and scss is behaving as intended. awaiting routing when search button is clicked

--- Proj2 - Video Game Page/ng-video-game-db/src/app/components/search-bar/search-bar.component.ts
+++ Proj2 - Video Game Page/ng-video-game-db/src/app/components/search-bar/search-bar.component.ts
@@ -9,7 +9,6 @@ import { Router } from '@angular/router';
 })
 export class SearchBarComponent implements OnInit {
 
-  // constructor() { }
   constructor(private router: Router) { }
 
   ngOnInit(): void {
