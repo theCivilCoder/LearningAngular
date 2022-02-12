922692f6676b818f3ee83a3e6b29aa4e8a5230ef
theCivilCoder <yongjunjz@hotmail.com>
2022-02-12 13:27:49 -0700

    Implemented the sort menu to extract the value whenever a new option is selected. service was already setup to use the sort variable

--- Proj2 - Video Game Page/ng-video-game-db/src/app/components/home/home.component.ts
+++ Proj2 - Video Game Page/ng-video-game-db/src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { APIResponse, Game } from 'src/app/models';
 import { HttpService } from 'src/app/services/http.service';
 
@@ -11,6 +12,8 @@ import { HttpService } from 'src/app/services/http.service';
 export class HomeComponent implements OnInit {
   public sort: string;
   public games: Array<Game>;
+  private routeSub: Subscription;
+  private gameSub: Subscription;
 
   constructor(
     private httpService: HttpService,
@@ -18,7 +21,7 @@ export class HomeComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe( (params: Params) =>{
+    this.routeSub = this.activatedRoute.params.subscribe( (params: Params) =>{
       if (params['game-search']){
         this.searchGames('metacrit', params['game-search']);
       } else {
@@ -28,12 +31,22 @@ export class HomeComponent implements OnInit {
   }
 
   searchGames(sort:string, search?: string){
-    this.httpService
+    this.gameSub = this.httpService
       .getGameList(sort, search)
       .subscribe((gameList: APIResponse<Game>) =>{
         this.games = gameList.results;
         console.log(gameList);
       });
+  }
+
+  ngOnDestroy(): void {
+    if (this.gameSub){
+      this.gameSub.unsubscribe();
+    }
+    
+    if (this.routeSub){
+      this.routeSub.unsubscribe();
+    }
   }
 
 }
