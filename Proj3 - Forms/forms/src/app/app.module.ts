bb98a72876ec36f60d45ded915f0c7907c00e429
theCivilCoder <yongjunjz@hotmail.com>
2022-02-15 13:24:06 -0700

    installed angular materials and imported the appropriate modules for the dialog (popup), toolbar, and icons

--- Proj3 - Forms/forms/src/app/app.module.ts
+++ Proj3 - Forms/forms/src/app/app.module.ts
@@ -5,18 +5,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
+import { EndPageComponent } from './end-page/end-page.component';
+import { DataTablesModule } from "angular-datatables";
+import { PopupDialogComponent } from './popup-dialog/popup-dialog.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent
+    FormComponent,
+    EndPageComponent,
+    PopupDialogComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    DataTablesModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  // entryComponents //depracated
 })
 export class AppModule { }
