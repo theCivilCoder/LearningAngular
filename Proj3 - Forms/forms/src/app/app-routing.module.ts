2ba846c71ec6ac570785f71be064a86bf57c569d
theCivilCoder <yongjunjz@hotmail.com>
2022-02-16 17:17:55 -0700

    Working modal example using ng-bootstrap from source website

--- Proj3 - Forms/forms/src/app/app-routing.module.ts
+++ Proj3 - Forms/forms/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EndPageComponent } from './end-page/end-page.component';
 import { FormComponent } from './form/form.component';
+import { ModalComponentComponent } from './modal-component/modal-component.component';
 import { PopupDialogComponent } from './popup-dialog/popup-dialog.component';
 
 const routes: Routes = [
   {path:"", component:FormComponent},
   {path:"end", component:EndPageComponent},
-  {path:"popup", component:PopupDialogComponent}
+  {path:"popup", component:PopupDialogComponent},
+  {path:"modal", component:ModalComponentComponent},
 ];
 
 @NgModule({
