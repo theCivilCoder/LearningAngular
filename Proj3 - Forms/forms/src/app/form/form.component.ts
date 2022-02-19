e866e578c4d815bf0484a80e08d384493b9edbd7
theCivilCoder <yongjunjz@hotmail.com>
2022-02-19 09:31:19 -0700

    Created a table with a modified column width for the checkbox column. Also implemented logic to identify which checkboxes were selected so that each selected row can be extracted in the ts file

--- Proj3 - Forms/forms/src/app/form/form.component.ts
+++ Proj3 - Forms/forms/src/app/form/form.component.ts
@@ -8,15 +8,34 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
+  arrEmails = [
+    {address:'email1', name:'a', gender: 'M', company:'ABC'},
+    {address:'email2', name:'b', gender: 'M', company:'ABC'},
+    {address:'email3', name:'c', gender: 'M', company:'ABC'},
+  ];
 
-  constructor( ) { }
+  arrSelection = Array(this.arrEmails.length).fill(false);
+
+  constructor( ) {   }
 
   ngOnInit(): void {
   }
 
   onSubmit(form: NgForm){
     console.log(form.value.year)
+
+    let arrRecipients: Object[] = [];
+    this.arrSelection.forEach( (isChecked, index) =>{
+      if (isChecked) arrRecipients.push(this.arrEmails[index])
+    } )
+
     console.log("clicked on submit")
+    console.log(arrRecipients)
+  }
+
+  toggle(i: number){
+    this.arrSelection[i] = !this.arrSelection[i]
+    console.log(this.arrSelection)
   }
 
   
