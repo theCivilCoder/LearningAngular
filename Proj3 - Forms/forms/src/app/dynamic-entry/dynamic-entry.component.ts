1a519143a3a3ba9a11c6c721238f056906b77a16
theCivilCoder <yongjunjz@hotmail.com>
2022-02-24 08:09:45 -0700

    Can programmatically add new components onto the html

--- Proj3 - Forms/forms/src/app/dynamic-entry/dynamic-entry.component.ts
+++ Proj3 - Forms/forms/src/app/dynamic-entry/dynamic-entry.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ControlContainer } from '@angular/forms';
 
 @Component({
-  selector: 'app-dynamic-entry',
+  selector: '[formGroupName] app-dynamic-entry',
   templateUrl: './dynamic-entry.component.html',
   styleUrls: ['./dynamic-entry.component.scss', '../dynamic-form/dynamic-form.component.scss']
 })
 export class DynamicEntryComponent implements OnInit {
-
-  constructor() { }
+  public questionForm : any 
+  constructor(private controlContainer: ControlContainer) { }
 
   ngOnInit(): void {
+    this.questionForm = this.controlContainer.control;
   }
 
 }
--- Proj3 - Forms/forms/src/app/dynamic-form/dynamic-form.component.ts
+++ Proj3 - Forms/forms/src/app/dynamic-form/dynamic-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { DynamicEntryComponent } from '../dynamic-entry/dynamic-entry.component';
 
 @Component({
@@ -8,17 +9,36 @@ import { DynamicEntryComponent } from '../dynamic-entry/dynamic-entry.component'
 })
 export class DynamicFormComponent implements OnInit {
   index = 0;
+  idxString = '';
   arrEntries: number[] = [];
+  arrFormGroupNames: string[] = [];
+  // arrEntryPartNames: string[] = ['Question', 'Category', 'Category Group', 'Weight'];
 
-  constructor() {
+  public form: FormGroup = this.formBuilder.group({})
+
+  constructor(private formBuilder: FormBuilder) {
   }
 
   ngOnInit(): void {
   }
 
   addNewEntry(){
+    // console.log('latest num is '+num.toString)
     this.arrEntries.push(this.index)
     this.index++
+    // console.log(('Q'+this.index.toString()))
+    this.arrFormGroupNames.push('Q'+this.index.toString())
+    let idxString = this.index.toString()
+    this.form.addControl(('Q'+idxString), this.formBuilder.group({
+      'question':'',
+      'category':'',
+      'categoryGroup':'',
+      'weight':'',
+      'answer':''
+    }))
+    console.log('the current form: ')
+    console.log(this.form.getRawValue())
+    // console.log(this.arrFormGroupNames)
   }
 
 
